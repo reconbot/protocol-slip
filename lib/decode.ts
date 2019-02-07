9784e666c4f41a3c241139683c5cb2a4fc0ec187
Francis Gulotta <wizard@roborooter.com>
2019-02-06 23:35:06 -0500

    Make sync decode a non generator iterable (same perf afaict)

--- lib/decode.ts
+++ lib/decode.ts
@@ -60,19 +60,88 @@ function* packetDecoder(bufferSize = 1024) {
   }
 }
 
-function* _syncDecode(iterable: Iterable<Buffer>) {
-  const decoder = packetDecoder()
-  decoder.next() // start it up
-  for (const data of iterable) {
-    const packets = decoder.next(data).value
-    if (packets) {
-      yield* packets
+function _syncDecode(iterable: Iterable<Buffer>): IterableIterator<Buffer> {
+  const bufferSize = 1024
+  let buffer = Buffer.allocUnsafe(bufferSize)
+  let input: Buffer = Buffer.alloc(0)
+  let writeCursor = 0
+  let inputCursor = 0
+  let prevEscape = false
+  let done = false
+  const itr = iterable[Symbol.iterator]()
+
+  const writeByte = (byte: number) => {
+    if (writeCursor >= buffer.length) {
+      buffer = Buffer.concat([buffer, Buffer.allocUnsafe(bufferSize)])
+    }
+    buffer[writeCursor] = byte
+    writeCursor++
+  }
+
+  const returnPacket = () => {
+    const packet = buffer.slice(0, writeCursor)
+    buffer = buffer.slice(writeCursor)
+    writeCursor = 0
+    return { done: false, value: packet }
+  }
+
+  function next(): IteratorResult<Buffer> {
+    if (done) {
+      return { done, value: undefined as any }
+    }
+
+    while (true) {
+      if (inputCursor >= input.length) {
+        const result = itr.next()
+        if (result.done) {
+          done = true
+          if (writeCursor > 0) {
+            return returnPacket()
+          } else {
+            return { done: true, value: undefined as any }
+          }
+        } else {
+          input = result.value
+          inputCursor = 0
+        }
+      }
+
+      while (inputCursor < input.length) {
+        const byte = input[inputCursor++]
+        switch (byte) {
+          case ESC[0]:
+            prevEscape = true
+            break
+          case END[0]:
+            prevEscape = false
+            if (writeCursor > 0) {
+              return returnPacket()
+            }
+            break
+          default:
+            if (!prevEscape) {
+              writeByte(byte)
+            } else {
+              prevEscape = false
+              let newValue = byte
+              if (byte === ESC_END[0]) {
+                newValue = END[0]
+              } else if (byte === ESC_ESC[0]) {
+                newValue = ESC[0]
+              }
+              writeByte(newValue)
+            }
+        }
+      }
     }
   }
-  const lastPackets = decoder.next(null).value
-  if (lastPackets) {
-    yield* lastPackets
+
+  const iterableIterator = {
+    next,
+    [Symbol.iterator]: () => iterableIterator,
   }
+
+  return iterableIterator
 }
 
 async function* _asyncDecode(iterable: AsyncIterable<Buffer>) {
